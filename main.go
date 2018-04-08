27f8ae1dbb89789093b5f473fda0c43745318b4c
rosshpayne <rosshpayne@gmail.com>
2018-04-08 14:27:38 +1000

    new private ip

--- main.go
+++ main.go
@@ -43,7 +43,8 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	     log.Printf("StageVariable:  %s  %v",k,v)
         }
 
-	dgraph := string("ip-172-31-17-148.ap-southeast-2.compute.internal:9080")
+	//dgraph := string("ip-172-31-17-148.ap-southeast-2.compute.internal:9080")
+	dgraph := string("ip-172-31-37-53.ap-southeast-2.compute.internal:9080")
 	    
 	conn, err := grpc.Dial(dgraph, grpc.WithInsecure())
 	if err != nil {
